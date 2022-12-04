b36a2298d7e1b7374125a44148adadaf1f36fb13
Kyle Serebour <kyle.serebour@symetris.ca>
2022-12-04 02:35:18 -0500

    Big tests.

---
+++ web/themes/custom/dreamland_theme/harmony/entity.node.page.harmony.php
@@ -0,0 +1,26 @@
+<?php
+
+use Drupal\harmonize\Service\Harmonics;
+
+/**
+ * @file
+ * Override & customize data harmonizations for a given Drupal object or entity.
+ *
+ * @harmonizerType entity
+ * @entityType node
+ * @bundle page
+ *
+ * This is a custom file to handle the reformatting of data before it reaches
+ * Drupal templates.
+ *
+ * Available variables:
+ * @var array $consignment
+ *    The data consigned by core Harmonize behaviour or Refiners.
+ * @var array $harmony
+ *    The data that will be returned to the Twig template.
+ * @var Harmonics $harmonics
+ *    The Harmonics service which contains helper functions.
+ *
+ * Re-arrange and assign data to the $harmony variable here, and that's all you
+ * need. The rest will be handled in the Twig Templates.
+ */
---
+++ web/themes/custom/dreamland_theme/js/dreamland-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Dreamland Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.dreamlandTheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
